2daf446688f821915824b84b4904178750f6cf84
Ryan <rashley@iqt.org>
2021-02-26 09:08:27 -0800

    refactor to more modular architecture

--- flickr/src/config.py
+++ flickr/src/config.py
@@ -7,6 +7,7 @@ def parse_config(cfg_file='./config.yaml'):
         for city in parsed["cities"]:
             city_cfg={}
             city_cfg["bounding_boxes"] = city["city"]["bounding_boxes"]
+            city_cfg["download"] = city["city"]["download"]
             effective_cfg[city["city"]['name']] = city_cfg
 
     return effective_cfg
--- flickr/src/flickr_search.py
+++ flickr/src/flickr_search.py
@@ -1,4 +1,5 @@
 import os
+
 from flickrapi import FlickrAPI
 from pprint import pprint
 
@@ -11,30 +12,36 @@ def get_secret(secret_name):
     except IOError:
         return None
 
-FLICKR_PUBLIC = get_secret('flickr_api_key')
-FLICKR_SECRET = get_secret('flickr_api_secret')
+def get_metadata(cfg):
+    FLICKR_PUBLIC = get_secret('flickr_api_key')
+    FLICKR_SECRET = get_secret('flickr_api_secret')
 
-PRIVACY_FILTER = 1 #only public photos
-CONTENT_TYPE = 1 #only photos
-HAS_GEO = True  #only geotagged photos
-GEO_CTX = 2     #flickr api values looking for outdoor images only
+    PRIVACY_FILTER = 1 #only public metadata
+    CONTENT_TYPE = 1 #only metadata
+    HAS_GEO = True  #only geotagged metadata
+    GEO_CTX = 2     #flickr api values looking for outdoor images only
 
-#DC=[[[-77.0990375584,38.8024915206],[-76.9067768162,38.8024915206],[-76.9067768162,39.0012743239],[-77.0990375584,39.0012743239],[-77.0990375584,38.8024915206]]]
-cfg = parse_config('../config.yaml')
-flickr = FlickrAPI(FLICKR_PUBLIC, FLICKR_SECRET, format='parsed-json')
+    flickr = FlickrAPI(FLICKR_PUBLIC, FLICKR_SECRET, format='parsed-json')
 
-extras ='description,license,date_upload,date_taken,original_format,'
-extras+='last_update,geo,tags, machine_tags, o_dims, media,'
-extras+='url_m,url_n,url_z,url_c,url_l,url_o'
+    extras ='description,license,date_upload,date_taken,original_format,'
+    extras+='last_update,geo,tags, machine_tags, o_dims, media,'
+    extras+='url_m,url_n,url_z,url_c,url_l,url_o'
 
-# bounds = '-77.0990375584, 38.8024915206, -76.9067768162, 39.0012743239'
-photos = {}
-for key in cfg:
-    print(f'{key}')
-    photos[key]=[]
-    for idx, bbox in enumerate(cfg[key]['bounding_boxes']):
-        city_pics = flickr.photos.search(privacy_filter=PRIVACY_FILTER, bbox=bbox, content_type=CONTENT_TYPE,
-        has_geo=HAS_GEO, geo_context=GEO_CTX, extras=extras, per_page=100)
-        photos[key].insert(idx,city_pics['photos'])
+    metadata = {}
+    for key in cfg:
+        print(f'{key}')
+        metadata[key]=[]
+        for idx, bbox in enumerate(cfg[key]['bounding_boxes']):
+            city_pics = flickr.photos.search(privacy_filter=PRIVACY_FILTER, bbox=bbox, content_type=CONTENT_TYPE,
+            has_geo=HAS_GEO, geo_context=GEO_CTX, extras=extras, per_page=100)
+            metadata[key].insert(idx,city_pics['photos'])
 
-pprint(photos)
+    return metadata
+
+def main(config_file):
+    config = parse_config(config_file)
+    metadata = get_metadata(config)
+    pprint(metadata)
+
+if __name__ == '__main__':  # pragma: no cover
+    main('../config.yaml')
