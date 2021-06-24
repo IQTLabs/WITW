8975ccb66d241e625a5cac10984a67c2abd35b98
Daniel Hogan <6313241+dphogan@users.noreply.github.com>
2021-06-23 22:58:02 -0400

    Control sitetiles.py operations from command line

--- tools/sitetiles.py
+++ tools/sitetiles.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import sys
 import csv
 import json
 import tqdm
@@ -172,7 +173,7 @@ if __name__ == '__main__':
     json_dir = '/local_data/geoloc/terrestrial/metadata'
     csv_path = '/local_data/geoloc/dataset.csv'
 
-    if True: # JSON METADATA -> CSV FILE
+    if 'csv' in sys.argv[1:]: # JSON METADATA -> CSV FILE
         dfs = []
         for aoi in range(1,1+11):
             path = os.path.join(json_dir, names[aoi-1], 'metadata.json')
@@ -194,7 +195,7 @@ if __name__ == '__main__':
 
         df.to_csv(csv_path, index=False, quoting=csv.QUOTE_NONNUMERIC)
 
-    if True: # CSV FILE -> DATASET
+    if 'dataset' in sys.argv[1:]: # CSV FILE -> DATASET
         df = csv_to_dataframe(csv_path)
         # download(df)
-        # clip(df)
+        clip(df)
