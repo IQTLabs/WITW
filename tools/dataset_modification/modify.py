a7c31cca454448ad23d07f19aa3eb6582a165c61
Daniel Hogan <6313241+dphogan@users.noreply.github.com>
2021-09-01 17:55:06 -0400

    Function to read an image file

--- tools/dataset_modification/modify.py
+++ tools/dataset_modification/modify.py
@@ -1,24 +1,62 @@
 #!/usr/bin/env python
 
 import os
-#import sys
 import argparse
+from skimage import io
 
-#sys.path.append('../../model')
-#import cvig_fov as cvig
-#Globals = cvig.Globals
-#device = cvig.device
+def file_to_batch(path):
+    """
+    Convert a single image file to batch of size 1
+    """
+    raw = io.imread(path)
+    image = torch.from_numpy(raw.astype(np.float32).transpose((2, 0, 1)))
+    batch = torch.unsqueeze(0) # Convert image to batch of size 1
+    return batch
+
+def batch_to_file(batch, path):
+    """
+    Convert a batch of size 1 to image file
+    """
+    pass
 
 
-def main(option, surface_in, overhead_in, surface_out, overhead_out):
+def main(options, surface_in, overhead_in, surface_out, overhead_out):
 
-    # Set up input
-    surface_paths = os.listdir(surface_in)
-    overhead_paths = os.listdir(overhead_in)
-
-    # Set up output
+    # Setup
+    surface_names = os.listdir(surface_in)
+    overhead_names = os.listdir(overhead_in)
     os.makedirs(surface_out, exist_ok=True)
     os.makedirs(overhead_out, exist_ok=True)
+
+    # Loop through views, then through images
+    # Execute selected options
+    for view in ['surface', 'overhead']:
+        if view == 'surface':
+            names = surface_names
+            dir_in = surface_in
+            dir_out = surface_out
+        else:
+            names = overhead_names
+            dir_in = overhead_in
+            dir_out = overhead_out
+        for name in names:
+            path_in = os.path.join(dir_in, name)
+            path_out = os.path.join(dir_out, name)
+            print(path_in)
+            print(path_out)
+            print()
+
+
+
+    """
+    for option in options:
+        print('Option', option)
+        if option == 0: # Filler; does nothing
+            pass
+        elif option == 1:
+            pass
+    """
+
 
 if __name__ == '__main__':
 
@@ -28,9 +66,10 @@ if __name__ == '__main__':
     default_overhead_out = '/local_data/geoloc/cvusamod/streetview/panos'
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('option',
-                        default=0,
-                        help='Image modification option')
+    parser.add_argument('options',
+                        nargs='*',
+                        type=int,
+                        help='Image modification options')
     parser.add_argument('--surface_in',
                         default=default_surface_in,
                         help='Source directory for surface images')
@@ -44,5 +83,5 @@ if __name__ == '__main__':
                         default=default_overhead_out,
                         help='Output directory for overhead images')
     args = parser.parse_args()
-    main(args.option, args.surface_in, args.overhead_in,
+    main(args.options, args.surface_in, args.overhead_in,
          args.surface_out, args.overhead_out)
