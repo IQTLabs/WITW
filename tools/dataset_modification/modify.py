b62d67ca65533990066f8d5357da10cb5bdc8af0
Daniel Hogan <6313241+dphogan@users.noreply.github.com>
2021-09-01 14:53:00 -0400

    modify.py arguments with only file paths

--- tools/dataset_modification/modify.py
+++ tools/dataset_modification/modify.py
@@ -1,43 +1,48 @@
 #!/usr/bin/env python
 
-import sys
+import os
+#import sys
 import argparse
 
-sys.path.append('../../model')
-import cvig_fov as cvig
+#sys.path.append('../../model')
+#import cvig_fov as cvig
 #Globals = cvig.Globals
 #device = cvig.device
 
 
-def main(option, dataset_format, csv_path, surface_dir, overhead_dir):
+def main(option, surface_in, overhead_in, surface_out, overhead_out):
 
-    dataset = cvig.ImagePairDataset(dataset_format, csv_path)
-    os.makedirs(surface_dir, exist_ok=True)
-    os.makedirs(overhead_dir, exist_ok=True)
+    # Set up input
+    surface_paths = os.listdir(surface_in)
+    overhead_paths = os.listdir(overhead_in)
+
+    # Set up output
+    os.makedirs(surface_out, exist_ok=True)
+    os.makedirs(overhead_out, exist_ok=True)
 
 if __name__ == '__main__':
 
-    default_dataset = 'cvusa'
-    default_csv = '/local_data/cvusa/all-19zl.csv'
-    default_surface = '/local_data/geoloc/cvusamod/bingmap/19'
-    default_overhead = '/local_data/geoloc/cvusamod/streetview/panos'
+    default_surface_in = '/local_data/cvusa/bingmap/19'
+    default_overhead_in = '/local_data/cvusa/streetview/panos'
+    default_surface_out = '/local_data/geoloc/cvusamod/bingmap/19'
+    default_overhead_out = '/local_data/geoloc/cvusamod/streetview/panos'
 
     parser = argparse.ArgumentParser()
     parser.add_argument('option',
                         default=0,
                         help='Image modification option')
-    parser.add_argument('--dataset',
-                        default=default_dataset,
-                        choices=['cvusa', 'witw'],
-                        help='Dataset format')
-    parser.add_argument('--csv',
-                        default=default_csv,
-                        help='Path to source dataset\'s CSV file')
-    parser.add_argument('--surface',
-                        default=default_surface,
-                        help='Directory to save surface images')
-    parser.add_argument('--overhead',
-                        default=default_overhead,
-                        help='Directory to save overhead images')
+    parser.add_argument('--surface_in',
+                        default=default_surface_in,
+                        help='Source directory for surface images')
+    parser.add_argument('--overhead_in',
+                        default=default_overhead_in,
+                        help='Source directory for overhead images')
+    parser.add_argument('--surface_out',
+                        default=default_surface_out,
+                        help='Output directory for surface images')
+    parser.add_argument('--overhead_out',
+                        default=default_overhead_out,
+                        help='Output directory for overhead images')
     args = parser.parse_args()
-    main(args.option, args.dataset, args.csv, args.surface, args.overhead)
+    main(args.option, args.surface_in, args.overhead_in,
+         args.surface_out, args.overhead_out)
