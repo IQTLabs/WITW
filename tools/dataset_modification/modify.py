9810b63296f2adad6e387d91f8b4cbe5ad9e29ef
Daniel Hogan <6313241+dphogan@users.noreply.github.com>
2021-09-01 23:12:26 -0400

    Converting to saving with skimage

--- tools/dataset_modification/modify.py
+++ tools/dataset_modification/modify.py
@@ -14,7 +14,8 @@ def file_to_batch(path):
     Convert a single image file to batch of size 1
     """
     raw = io.imread(path)
-    image = torch.from_numpy(raw.astype(np.float32).transpose((2, 0, 1)))
+    #image = torch.from_numpy(raw.astype(np.float32).transpose((2, 0, 1)))
+    image = torch.from_numpy(raw.transpose((2, 0, 1)))
     batch = image.unsqueeze(0)
     return batch
 
@@ -24,7 +25,9 @@ def batch_to_file(batch, path):
     Convert a batch of size 1 to image file
     """
     image = torch.squeeze(batch, dim=0)
-    torchvision.utils.save_image(image, path)
+    raw = image.numpy().transpose((1, 2, 0))
+    io.imsave(path, raw)
+    #torchvision.utils.save_image(image, path, value_range=(0,255))
 
 
 def main(options, surface_in, overhead_in, surface_out, overhead_out):
@@ -44,45 +47,15 @@ def main(options, surface_in, overhead_in, surface_out, overhead_out):
         if 1 in options:
             batch_to_file(surface, os.path.join(surface_out, name))
         if 2 in options:
-            batch_to_file(surface, os.path.join(surface_out, name))
-        
-    """
-    # Loop through views, then through images
-    # Execute selected options
-    for view in ['surface', 'overhead']:
-        if view == 'surface':
-            names = surface_names
-            dir_in = surface_in
-            dir_out = surface_out
-        else:
-            names = overhead_names
-            dir_in = overhead_in
-            dir_out = overhead_out
-        for name in names:
-            path_in = os.path.join(dir_in, name)
-            path_out = os.path.join(dir_out, name)
-
-            # Load input images
-            surface = file_to_batch(path)
-            print(path_in)
-            print(path_out)
-            print()
-
-    for option in options:
-        print('Option', option)
-        if option == 0: # Filler; does nothing
-            pass
-        elif option == 1:
-            pass
-    """
+            batch_to_file(overhead, os.path.join(overhead_out, name))
 
 
 if __name__ == '__main__':
 
-    default_surface_in = '/local_data/cvusa/bingmap/19'
-    default_overhead_in = '/local_data/cvusa/streetview/panos'
-    default_surface_out = '/local_data/geoloc/cvusamod/bingmap/19'
-    default_overhead_out = '/local_data/geoloc/cvusamod/streetview/panos'
+    default_surface_in = '/local_data/cvusa/streetview/panos'
+    default_overhead_in = '/local_data/cvusa/bingmap/19'
+    default_surface_out = '/local_data/geoloc/cvusamod/streetview/panos'
+    default_overhead_out = '/local_data/geoloc/cvusamod/bingmap/19'
 
     parser = argparse.ArgumentParser()
     parser.add_argument('options',
